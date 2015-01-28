91ad0f20492f6d06a7e21bcc13ce13bdfe0da159
karandani <karan.dani@spec-india.com>
2015-01-28 16:40:12 +0530

    Karan sunilkumar Dani

--- GitDemo/src/com/demo/git/first/GitDemo.java
+++ GitDemo/src/com/demo/git/first/GitDemo.java
@@ -3,6 +3,6 @@ package com.demo.git.first;
 public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("This IS My  sdfb dfhdfh First Git sadfasf sdfsdf Program");
-		System.out.println("Karan Dani");
+		System.out.println("Karan sunilkumar Dani");
 	}
 }
